f5ff9a4115ec3bf0e13f00c05beaf3018b500dd8
gireeshch275-ai <gireeshch275@gmail.com>
2026-09-04 14:21:25 +0530

    operators in python.py, identity operators.py ,logical operators.py

---
+++ logical operators.py
@@ -0,0 +1,12 @@
+#a=30
+#result = a > 20 and a < 9
+#print("result of",a,"> 20 and", a ,"< 9: ", result)
+
+
+a = 6
+
+print(a < 8 or a > 8)
+
+result = a < 8 and a > 8
+
+print("Result of", a, "< 8 and", a, "> 8:", result)
---
+++ operators in python.py
@@ -0,0 +1,7 @@
+a=10
+b=5
+result =a > b
+print("result of",a, ">",b,"is : ", result)
+
+result = a < b
+print("result of",a, "<",b,"is : ", result)
