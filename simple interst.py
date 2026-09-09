54c85f5e910120a618bff036a2fac5874c07e362
gireeshch275-ai <gireeshch275@gmail.com>
2026-09-09 11:49:41 +0530

    simple interest

---
+++ simple interst.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter principal amount: "))
+time = float(input("Enter the time (in years): "))
+rate = float(input("Enter rate: "))
+
+simple_interest = (principal * rate * time) / 100
+
+print("Simple Interest =", simple_interest)
